66ff012d5422b33315fd7f29d20abce413710227
RasimGuziev969 <zguzievrasim@gmail.com>
2023-08-06 13:37:57 +0300

    Task 2-5

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Transform/LeftShiftTransformer.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace PhotoEnhancer
+{
+    public class LeftShiftTransformer : ITransformer<LeftShiftParameters>
+    {
+        public Size ResultSize { get; private set; }
+        private LeftShiftParameters _parameters;
+        private int shiftPixels;
+
+        public void Initialize(Size size, LeftShiftParameters parameters)
+        {
+            this.ResultSize = size;
+            this._parameters = parameters;
+            this.shiftPixels =  (int)(this._parameters.Shift * ResultSize.Width / 100);
+        }
+
+        public Point? MapPoint(Point newPoint)
+        {
+            return new Point((newPoint.X + shiftPixels) %ResultSize.Width, newPoint.Y);
+        }
+    }
+}
---
+++ PhotoEnhancer/PhotoEnhancer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using PhotoEnhancer.Filters;
+
+namespace PhotoEnhancer
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var mainForm = new MainForm();
+
+            mainForm.AddFilter(new PixelFilter<LighteningParameters>(
+                "Осветление/затемнение",
+                (pixel, parameters) => pixel * parameters.Coefficient));
+            
+            mainForm.AddFilter(new PixelFilter<EmptyParameters>(
+                "Оттенки серого",
+                (pixel, parameters) =>
+                {
+                    var lightness = 0.3 * pixel.R + 0.6 * pixel.G + 0.1 * pixel.B;
+                    return new Pixel(lightness, lightness, lightness);
+                }));
+            mainForm.AddFilter(new PixelFilter<TrasholdParameters>(
+                "Порог",
+                (pixel, parameters) =>
+                {
+                    var lightness = 0.3 * pixel.R + 0.6 * pixel.G + 0.1 * pixel.B;
+                    var res = (lightness < (parameters.Trashold)) ? 0 : 1;
+                    return new Pixel(res, res, res);
+                }));
+            mainForm.AddFilter(new TransformFilter(
+                "Отражение по горизонтали",
+                size => size,
+                (point, size) => new Point(size.Width - point.X - 1, point.Y)
+                ));
+            mainForm.AddFilter(new TransformFilter(
+               "Отражение по вертикали",
+               size => size,
+               (point, size) => new Point(point.X, size.Height - point.Y - 1)
+               ));
+
+            mainForm.AddFilter(new TransformFilter(
+                "Поворот на 90° против ч. с.",
+                size => new Size(size.Height, size.Width),
+                (point, size) => new Point(size.Width - point.Y - 1, point.X)
+                ));
+
+            mainForm.AddFilter(new TransformFilter<RotationParameters>(
+                "Поворот на произвольный угол",
+                new RotateTransformer()
+                ));
+            mainForm.AddFilter(new TransformFilter<LeftShiftParameters>(
+             "Сдвиг влево",
+             new LeftShiftTransformer()
+             ));
+            Application.Run(mainForm);
+        }
+    }
+}
